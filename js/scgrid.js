fd0b79ab5a8161c4b47769fe1045cfbcadfd2dd0
Jon <jon@feddev5.ddi.sheridanc.on.ca>
2014-05-22 15:21:11 -0400

    Starting to actually look like a grid

--- js/scgrid.js
+++ js/scgrid.js
@@ -5,11 +5,6 @@ var SC_BASE_URL = "https://api.soundcloud.com";
 
 $(document).ready(function() {
 
-
-
-
-
-
   console.log("page loaded");
 
 
@@ -25,20 +20,44 @@ function fetchSoundCloudTracks() {
   $.ajax({
     url: SC_BASE_URL + "/tracks",
     context: document.body,
-    // dataType: 'xml',
+    dataType: 'xml',
     data: {
       'client_id': CLIENT_ID,
       'limit': 200
     },
-    success: function(data) {
 
-      console.log("Fetching tracks: Success");
+    beforeSend: function() {
+      console.log("Fetching tracks...");
+    },
+
+    success: function(data) {
+      console.log("Tracks fetched!");
       console.log(data);
 
-    },
-    error: function(jqXHR, textStatus, errorThrown) {
+      $(data).find("tracks").find("track").each(function() {
 
-      console.log("Fetching tracks failed...");
+        // Get the artwork for each track if it isn't null
+        if ($(this).find("artwork-url").attr("nil") != "true") {
+          $("#container").append("<img src='" + $(this).find("artwork-url").text() + "' />");
+        }
+
+        // If the track artwork is null, get the track uploader's profile picture
+        else {
+          $("#container").append("<img src='" + $(this).find("user").find("avatar-url").text() + "' />");
+
+          // We'll have to check here if the uploader has no profile picture
+          // in which case we'll probably just use a default image
+          // maybe the SoundCloud logo?
+
+
+        }
+
+      });
+
+    },
+
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log("Fetching tracks failed!");
       console.log("jqXHR: " + jqXHR);
       console.log("Status: " + textStatus);
       console.log("Error: " + errorThrown);
