b8c87b0b534dd6cf650bc456741a1908c12c26c0
Jon <jon@feddev5.ddi.sheridanc.on.ca>
2014-05-23 14:42:31 -0400

    Switched to SoundCloud's SDK

--- js/scgrid.js
+++ js/scgrid.js
@@ -2,13 +2,40 @@ var CLIENT_ID = "3bd54dc3bb72fedf2fbb3e415b8ba1bb";
 var SC_BASE_URL = "https://api.soundcloud.com";
 
 
+/*
+<iframe width="100%" height="400" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?visual=true&url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F293&show_artwork=true&auto_play=false"></iframe>
+*/
 
 $(document).ready(function() {
 
+  SC.initialize({
+    client_id: CLIENT_ID,
+    redirect_uri: "http://example.com/callback.html",
+  });
+
   console.log("page loaded");
+
+  console.log(SC);
+
+
+  SC.oEmbed("http://soundcloud.com/forss/flickermood", {auto_play: false}, function(oembed){
+     console.log("oEmbed response: ", oembed);
+
+     $("#track-player").append(oembed.html);
+
+   });
+
+  $(".grid-image").on("click", function(e) {
+
+    console.log("clicked!");
+
+  });
+
+
 
 
   fetchSoundCloudTracks();
+
 
 });
 
@@ -38,16 +65,18 @@ function fetchSoundCloudTracks() {
 
         // Get the artwork for each track if it isn't null
         if ($(this).find("artwork-url").attr("nil") != "true") {
-          $("#container").append("<img src='" + $(this).find("artwork-url").text() + "' />");
+          $("#container").append("<span class='grid-span'><img class='grid-image' src='" + $(this).find("artwork-url").text() + "' /></span>");
         }
 
         // If the track artwork is null, get the track uploader's profile picture
         else {
-          $("#container").append("<img src='" + $(this).find("user").find("avatar-url").text() + "' />");
+          $("#container").append("<span class='grid-span'><img class='grid-image' src='" + $(this).find("user").find("avatar-url").text() + "' /></span>");
 
           // We'll have to check here if the uploader has no profile picture
           // in which case we'll probably just use a default image
           // maybe the SoundCloud logo?
+
+
 
 
         }
@@ -66,6 +95,8 @@ function fetchSoundCloudTracks() {
 
 
   });
+
+
 
 
 
