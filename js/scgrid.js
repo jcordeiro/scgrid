28a46393c284ebcd37504b1a309e7b75f9c3b342
Jonathan Cordeiro <Jonathan0Cordeiro@gmail.com>
2014-05-24 02:18:20 -0400

    Changed player to show 'recent tracks'

--- js/scgrid.js
+++ js/scgrid.js
@@ -1,13 +1,9 @@
 var CLIENT_ID = "3bd54dc3bb72fedf2fbb3e415b8ba1bb";
 var SC_BASE_URL = "https://api.soundcloud.com";
 
-
-/*
-<iframe width="100%" height="400" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?visual=true&url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F293&show_artwork=true&auto_play=false"></iframe>
-*/
-
 $(document).ready(function() {
 
+  // Initialize SoundCloud SDK
   SC.initialize({
     client_id: CLIENT_ID,
     redirect_uri: "http://example.com/callback.html",
@@ -18,15 +14,6 @@ $(document).ready(function() {
   console.log(SC);
 
   fetchSoundCloudTracks();
-
-
-
-  // SC.oEmbed("http://soundcloud.com/forss/flickermood", {auto_play: false}, function(oembed){
-  //    console.log("oEmbed response: ", oembed);
-
-  //    $("#track-player").append(oembed.html);
-
-   // });
 
   $("#main-content").on("click", ".scgrid-image", function(e) {
 
@@ -39,29 +26,30 @@ $(document).ready(function() {
     var url = $(this).data("url");
     console.log(url);
 
-    SC.oEmbed(url, {auto_play: false}, function(oembed){
+    SC.oEmbed(url, {auto_play: true, color: '#0000FF'}, function(oembed){
        console.log("oEmbed response: ", oembed);
 
        $("#track-player").html(oembed.html);
-       $("#track-player").toggleClass("hidden");
+       // $("#track-player").toggleClass("hidden");
+
+       $("#track-player").fadeIn("fast");
+
+    });
+
+    $("#container").click(function(e) {
+
+      $("#track-player").fadeOut("slow");
+      console.log("closing..");
+
+
+      // Check if the track is playing
+      // so we know if we need to pause it
+
+      // TODO: Check if track is playing
+
     });
 
   });
-
-  // $(".scgrid-image").click(function(e) {
-
-  //   console.log("clicked!");
-
-  //   console.log(e);
-
-  //   console.log($(this));
-
-  // });
-
-
-
-
-
 
 });
 
@@ -93,18 +81,14 @@ function fetchSoundCloudTracks() {
 
       $(data).find("tracks").find("track").each(function() {
 
-        var a = $(this).find("permalink-url")[1];
-        console.log($(this).find(a).text());
+        var url = $(this).find("permalink-url")[0];
+        console.log($(this).find(url).text());
 
         // Get the artwork for each track if it isn't null
         if ($(this).find("artwork-url").attr("nil") != "true") {
           $("#main-content").append("<img class='scgrid-image'  src='" +
             $(this).find("artwork-url").text() +
-            // "' data-url='" + $(this).find("permalink-url")[1].text() +"' />");
-          "' data-url='" + $(this).find(a).text() +"' />");
-
-
-          // $("#container").append("<span class='grid-span pure-u-1-20' '><img class='grid-image '  src='" + $(this).find("artwork-url").text() + "' /></span>");
+          "' data-url='" + $(this).find(url).text() +"' />");
 
         }
 
@@ -112,19 +96,11 @@ function fetchSoundCloudTracks() {
         else {
           $("#main-content").append("<img class='scgrid-image' src='" +
             $(this).find("user").find("avatar-url").text() +
-             // "' data-url='" + $(this).find("permalink-url"[1]).text() +"' />");
-          "' data-url='" + $(this).find(a).text() +"' />");
-
-
-          // $("#container").append("<span class='grid-span pure-u-1-20 '><img class='grid-image  ' src='" + $(this).find("user").find("avatar-url").text() + "' /></span>");
-
+          "' data-url='" + $(this).find(url).text() +"' />");
 
           // We'll have to check here if the uploader has no profile picture
           // in which case we'll probably just use a default image
           // maybe the SoundCloud logo?
-
-
-
 
         }
 
@@ -144,12 +120,6 @@ function fetchSoundCloudTracks() {
       console.log("Error: " + errorThrown);
     }
 
-
-
   });
-
-
-
-
 
 }
