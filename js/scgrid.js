84b2b243fcb282f063b59f093fb9264e0d561276
Jonathan Cordeiro <Jonathan0Cordeiro@gmail.com>
2014-05-26 17:51:43 -0400

    Switched to SoundCloud's HTML5 Widget JS API

--- js/scgrid.js
+++ js/scgrid.js
@@ -3,51 +3,54 @@ var SC_BASE_URL = "https://api.soundcloud.com";
 
 $(document).ready(function() {
 
+  // Pauses and fades out the SoundCloud player widget
+  // when the user clicks anywhere else besides the player on the page
+  $(document).mouseup(function (e)
+  {
+      var player = $("#track-player");
+
+      if (!player.is(e.target) // if the target of the click isn't the container...
+          && player.has(e.target).length === 0) // ... nor a descendant of the container
+      {
+          // Pause the widget and fade it away
+          widget.pause();
+          player.fadeOut("slow");
+          console.log("closing..");
+      }
+  });
+
   // Initialize SoundCloud SDK
   SC.initialize({
     client_id: CLIENT_ID,
     redirect_uri: "http://example.com/callback.html",
   });
 
-  console.log("page loaded");
+  // Initialize the SoundCloud Player Widget
+  // and inject the iframe element into the page
+  $("#track-player").html('<iframe id="sc-widget"' +
+                          ' src="https://w.soundcloud.com/player/?url=' +
+                          'http://soundcloud.com/asma2fawzy' +
+                          '" width="100%" height="465" scrolling="no"' +
+                          'frameborder="no"></iframe>');
 
-  console.log(SC);
+  var widget = SC.Widget("sc-widget");
 
+  // Fetch the tracks from SoundCloud
   fetchSoundCloudTracks();
+
 
   $("#main-content").on("click", ".scgrid-image", function(e) {
 
-    console.log("clicked!");
-
-    console.log(e);
-
-    console.log($(this));
-
     var url = $(this).data("url");
+
+
     console.log(url);
 
-    SC.oEmbed(url, {auto_play: true, color: '#0000FF'}, function(oembed){
-       console.log("oEmbed response: ", oembed);
+    // Load the widget to play the selected song
+    widget.load(url, {auto_play: true});
 
-       $("#track-player").html(oembed.html);
-       // $("#track-player").toggleClass("hidden");
-
-       $("#track-player").fadeIn("fast");
-
-    });
-
-    $("#container").click(function(e) {
-
-      $("#track-player").fadeOut("slow");
-      console.log("closing..");
-
-
-      // Check if the track is playing
-      // so we know if we need to pause it
-
-      // TODO: Check if track is playing
-
-    });
+    // Display the widget
+    $("#track-player").fadeIn("fast");
 
   });
 
@@ -81,15 +84,13 @@ function fetchSoundCloudTracks() {
 
       $(data).find("tracks").find("track").each(function() {
 
-        var url = $(this).find("permalink-url")[0];
-        console.log($(this).find(url).text());
+        var url = $(this).find("uri")[0];
 
         // Get the artwork for each track if it isn't null
         if ($(this).find("artwork-url").attr("nil") != "true") {
           $("#main-content").append("<img class='scgrid-image'  src='" +
             $(this).find("artwork-url").text() +
           "' data-url='" + $(this).find(url).text() +"' />");
-
         }
 
         // If the track artwork is null, get the track uploader's profile picture
