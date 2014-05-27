7e15f5de02137db044949953f8161271bf6c09ff
Jonathan Cordeiro <Jonathan0Cordeiro@gmail.com>
2014-05-26 21:10:01 -0400

    Added default image for songs & artists without artwork

--- js/scgrid.js
+++ js/scgrid.js
@@ -5,31 +5,45 @@ $(document).ready(function() {
 
   // Pauses and fades out the SoundCloud player widget
   // when the user clicks anywhere else besides the player on the page
-  $(document).mouseup(function (e)
-  {
+  $(document).mouseup(function (e) {
       var player = $("#track-player");
 
-      if (!player.is(e.target) // if the target of the click isn't the container...
-          && player.has(e.target).length === 0) // ... nor a descendant of the container
-      {
-          // Pause the widget and fade it away
-          widget.pause();
-          player.fadeOut("slow");
-          console.log("closing..");
+      // Check that the player widget isn't already hidden
+      if ($("#track-player").is(":visible")) {
+        if (!player.is(e.target) && // if the target of the click isn't the container...
+            player.has(e.target).length === 0) // ... nor a descendant of the container
+        {
+            // Just pause the widget
+            widget.pause();
+
+
+            player.fadeOut("slow");
+
+
+            console.log("closing...");
+        }
+      }
+      else {
+        // Pause the widget and fade it away
+        widget.pause();
+        player.fadeOut("slow");
+
+        console.log("closing...");
+
       }
   });
 
   // Initialize SoundCloud SDK
   SC.initialize({
     client_id: CLIENT_ID,
-    redirect_uri: "http://example.com/callback.html",
+    redirect_uri: "http://www.joncordeiro.com/projects/scgrid",
   });
 
   // Initialize the SoundCloud Player Widget
   // and inject the iframe element into the page
   $("#track-player").html('<iframe id="sc-widget"' +
                           ' src="https://w.soundcloud.com/player/?url=' +
-                          'http://soundcloud.com/asma2fawzy' +
+                          'https%3A//api.soundcloud.com/tracks/27953592' + // Default URL before we load one the user clicks
                           '" width="100%" height="465" scrolling="no"' +
                           'frameborder="no"></iframe>');
 
@@ -43,19 +57,18 @@ $(document).ready(function() {
 
     var url = $(this).data("url");
 
+        widget.bind(SC.Widget.Events.READY, function() {
 
-    console.log(url);
-
-    // Load the widget to play the selected song
-    widget.load(url, {auto_play: true});
-
-    // Display the widget
-    $("#track-player").fadeIn("fast");
-
+            // Load the widget to play the selected song
+            widget.load(url, {auto_play: true,
+              callback: function() {
+              // Display the widget once it's loaded
+              $("#track-player").fadeIn("fast");
+            }
+          });
+        });
   });
-
 });
-
 
 // Performs the ajax call to soundcloud
 // and fetches an array of tracks
@@ -86,32 +99,37 @@ function fetchSoundCloudTracks() {
 
         var url = $(this).find("uri")[0];
 
+        var img_source = null;
+
         // Get the artwork for each track if it isn't null
         if ($(this).find("artwork-url").attr("nil") != "true") {
-          $("#main-content").append("<img class='scgrid-image'  src='" +
-            $(this).find("artwork-url").text() +
-          "' data-url='" + $(this).find(url).text() +"' />");
+          image_source = $(this).find("artwork-url").text();
         }
 
         // If the track artwork is null, get the track uploader's profile picture
         else {
-          $("#main-content").append("<img class='scgrid-image' src='" +
-            $(this).find("user").find("avatar-url").text() +
-          "' data-url='" + $(this).find(url).text() +"' />");
 
-          // We'll have to check here if the uploader has no profile picture
-          // in which case we'll probably just use a default image
-          // maybe the SoundCloud logo?
-
+          // Check that the uploader's profile picture isn't null
+          if ($(this).find("avatar-url").attr("nil") != "true") {
+            image_source = $(this).find("avatar-url").text();
+          }
+          else {
+            // If the uploader has not profile picture we'll just use
+            // a default image of the SoundCloud Logo
+            image_source = "/img/sclogo.png";
+          }
         }
 
+        // Add the image to the grid
+        $("#main-content").append("<img class='scgrid-image' src='" +
+        image_source +
+        "' data-url='" + $(this).find(url).text() +"' />");
       });
 
     },
     complete: function() {
       /* Hide ajax loader loader */
       $("#loader").toggleClass("hidden");
-
     },
 
     error: function(jqXHR, textStatus, errorThrown) {
